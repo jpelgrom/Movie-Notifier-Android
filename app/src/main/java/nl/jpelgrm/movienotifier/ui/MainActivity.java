1c1062a8b2b9cc77f64cc8ea13481203e52ae65a
Joris Pelgröm <joris.pelgrom@gmail.com>
2017-12-08 11:39:55 +0100

    Only re-schedule cinema list update job if absolutely necessary
     - Prevents resetting the timer on cinema update job every time the app is started

--- app/src/main/java/nl/jpelgrm/movienotifier/ui/MainActivity.java
+++ app/src/main/java/nl/jpelgrm/movienotifier/ui/MainActivity.java
@@ -16,6 +16,8 @@ import com.firebase.jobdispatcher.Job;
 import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.Trigger;
 
+import java.util.concurrent.TimeUnit;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import nl.jpelgrm.movienotifier.R;
@@ -99,19 +101,24 @@ public class MainActivity extends AppCompatActivity {
     private void setupCinemaListUpdates() {
         if(setupCinemaListUpdates) { return; }
 
+        long lastUpdated = settings.getLong("cinemasUpdated", -1);
         FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
-        Job updateJob = dispatcher.newJobBuilder()
-                .setService(CinemaUpdateJob.class)
-                .setTag("cinemasListUpdate")
-                .setRecurring(true)
-                .setLifetime(Lifetime.FOREVER)
-                .setTrigger(Trigger.executionWindow(0, 60 * 60 * 24 * 7 /* once a week */))
-                .setReplaceCurrent(true)
-                .build();
-        dispatcher.mustSchedule(updateJob);
+
+        if(lastUpdated < (System.currentTimeMillis() - TimeUnit.DAYS.toMillis(7))) {
+            // It's been more than a week since the last update, so setup/reset job because it should have run by now
+            Job updateJob = dispatcher.newJobBuilder()
+                    .setService(CinemaUpdateJob.class)
+                    .setTag("cinemasListUpdate")
+                    .setRecurring(true)
+                    .setLifetime(Lifetime.FOREVER)
+                    .setTrigger(Trigger.executionWindow(0, (int) TimeUnit.DAYS.toSeconds(7)))
+                    .setReplaceCurrent(true)
+                    .build();
+            dispatcher.mustSchedule(updateJob);
+        }
 
         // Also run immediately if the list has never been updated
-        if(settings.getLong("cinemasUpdated", -1) == -1) {
+        if(lastUpdated == -1) {
             dispatcher.mustSchedule(CinemaUpdateJob.getJobToUpdateImmediately(dispatcher));
         }
 
